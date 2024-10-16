2c47078573803195e1daad9b4d9cbaa140e4ef02
rithydragon <riththy.learn@gmail.com>
2024-10-16 07:21:48 +0700

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,20 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { hostname } from 'node:os'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
   ],
+  server: {
+    host: '0.0.0.0',  // Bind to all network interfaces (enable external access)
+    port: 5000,        // Port number
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
 })
